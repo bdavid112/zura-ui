8b35f0287432fee6c7170dd1743c88aac56bcd6f
bdavid112 <dawid8688@gmail.com>
2025-03-25 18:51:55 +0100

    Initial commit

---
+++ packages/zura-ui/src/dev.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/global.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <h1 className="mt-32">HELLO</h1>
+    <h2 className="mt-16">BELLO</h2>
+  </React.StrictMode>
+);
